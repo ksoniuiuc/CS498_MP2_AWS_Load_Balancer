963f6bfc02938fd41d044989d2021cb11461aa92
Kushagra Soni <soni.kushagra24@gmail.com>
2022-02-01 22:26:25 -0600

    Initial Commit

---
+++ flaskApp.py
@@ -0,0 +1,20 @@
+import sys
+from flask import Flask, request
+app = Flask(__name__)
+
+seed_value = "0"
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    global seed_value
+    if request.method == 'POST':
+        input = request.json
+        print(f'Input JSON: {input}')
+        seed_value = input.get('num')
+        print(f'Seed Value: {seed_value}')
+        return seed_value
+    
+    return seed_value
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
